ad82dc1d83ad2675e08efd48fe0c011b06eab761
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-09-25 19:12:12 +0500

    Employee

--- homework.js
+++ homework.js
@@ -55,34 +55,92 @@
 // Class
 
 //!class1
-class Triangle {
-  #leftSide;
-  #rightSide;
-  #width;
-  constructor(leftSide, rightSide, width) {
-    this.#leftSide = leftSide;
-    this.#rightSide = rightSide;
-    this.#width = width;
-  }
+// class Triangle {
+//   #leftSide;
+//   #rightSide;
+//   #width;
+//   constructor(leftSide, rightSide, width) {
+//     this.#leftSide = leftSide;
+//     this.#rightSide = rightSide;
+//     this.#width = width;
+//   }
 
-  getLeftSide() {
-    return this.#leftSide;
-  }
-  getRightSide() {
-    return this.#rightSide;
-  }
-  getWith() {
-    return this.#width;
-  }
-  setSides(a, b, c) {
-    this.#leftSide = a;
-    this.#rightSide = b;
-    this.#width = c;
-  }
-}
+//   getLeftSide() {
+//     return this.#leftSide;
+//   }
+//   getRightSide() {
+//     return this.#rightSide;
+//   }
+//   getWith() {
+//     return this.#width;
+//   }
+//   setSides(a, b, c) {
+//     this.#leftSide = a;
+//     this.#rightSide = b;
+//     this.#width = c;
+//   }
+// }
 
-let res = new Triangle(12, 13, 14);
-res.setSides(10, 20, 30);
-console.log(res.getLeftSide());
-console.log(res.getRightSide());
-console.log(res.getWith());
+// let res = new Triangle(12, 13, 14);
+// res.setSides(10, 20, 30);
+// console.log(res.getLeftSide());
+// console.log(res.getRightSide());
+// console.log(res.getWith());
+
+// !Class2
+
+// class Employee {
+//   #id;
+//   #firstName;
+//   #lastName;
+//   #salary;
+//   constructor(id, firstName, lastName, salary) {
+//     this.#id = id;
+//     this.#firstName = firstName;
+//     this.#lastName = lastName;
+//     this.#salary = salary;
+//   }
+
+//   getId() {
+//     return this.#id;
+//   }
+
+//   getFirstName() {
+//     return this.#firstName;
+//   }
+
+//   getLastName() {
+//     return this.#lastName;
+//   }
+//   getSalary() {
+//     return this.#salary;
+//   }
+
+//   setAll(id, fName, lName, s) {
+//     this.#id = id;
+//     this.#firstName = fName;
+//     this.#lastName = lName;
+//     this.#salary = s;
+//   }
+
+//   getFullName() {
+//     return this.#firstName + " " + this.#lastName;
+//   }
+//   getAnnualSalary() {
+//     return this.#salary * 12;
+//   }
+
+//   raiseSalary(percent) {
+//     return (this.#salary += (this.#salary * percent) / 100);
+//   }
+// }
+
+// let emp1 = new Employee(1, "John", "Doe", 5000);
+// emp1.setAll(2, "Adam", "Doe", 6000);
+// console.log(emp1.getId());
+// console.log(emp1.getFirstName());
+// console.log(emp1.getLastName());
+// console.log(emp1.getSalary());
+// console.log(emp1.getFullName());
+// console.log(emp1.getAnnualSalary());
+// console.log(emp1.raiseSalary(10));
